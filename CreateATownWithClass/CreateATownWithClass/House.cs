1476b32b86569e350ac49d2272516ad41a085a7d
nikolemc <nikolemcstanley@gmail.com>
2017-03-27 14:40:46 -0400

    Houses should have the Properties Name as a string BuildingType as string, that is set in the constructor NumberOfRoom as an int ConstructionMaterial as a string Height as double Width as double Length as Double Square Footage as a Read Only, calculated field Floors as integer BedRooms as integer BathRooms as integer Volume as a Read Only, calculated field FullAddress as a Read only, string value that is made of the Address1, Address2, city, state, zip Properties Current Occupants as read only integer IncreateOccupants as a public method that increments the CurrentOccupants by 1 DecreaseOccupants as a public method that decrements the CurrentOccupants by 1

--- CreateATownWithClass/CreateATownWithClass/House.cs
+++ CreateATownWithClass/CreateATownWithClass/House.cs
@@ -8,5 +8,59 @@ namespace CreateATownWithClass
 {
     class House
     {
+        public string PropertyName { get; set; }
+        public string BuildingType { get; set; }
+        public int NumberofRoom { get; set; }
+        public string ConstructionMaterial { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double Length { get; set; }
+        public double SquareFootage { get; }
+        public int Floors { get; set; }
+        public int Bedrooms { get; set; }
+        public int Bathrooms { get; set; }
+        public string Address1 { get; }
+        public string Address2 { get; }
+        public string City { get; }
+        public string State { get; }
+        public string Zip { get; }
+        public int CurrentOccupants { get { return _CurrentOccupants; } }
+        private int _CurrentOccupants;
+        
+
+
+
+        public string DisplayName()
+        {
+            return this.PropertyName;
+        }
+
+        public override string ToString()
+        {
+            return ($"{BuildingType} : {PropertyName}");
+
+        }
+
+        public string FullAddress
+        {
+            get
+            {
+                return Address1 + Address2 + City + State + Zip;
+            }
+        }
+        
+
+        public void IncreateOccupants()
+        {
+            this._CurrentOccupants++;
+
+        }
+
+        public void DecreaseOccupants()
+        {
+            this._CurrentOccupants--;
+
+        }
+
     }
 }
--- CreateATownWithClass/CreateATownWithClass/School.cs
+++ CreateATownWithClass/CreateATownWithClass/School.cs
@@ -8,5 +8,19 @@ namespace CreateATownWithClass
 {
     class School
     {
+        public string PropertyName { get; set; }
+        public string BuildingType { get; set; }
+
+        public string DisplayName()
+        {
+            return this.PropertyName;
+        }
+
+        public override string ToString()
+        {
+            return ($"{BuildingType} : {PropertyName}");
+
+        }
+
     }
 }
